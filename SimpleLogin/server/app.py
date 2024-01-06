ba546506c38cfed2945f44ee480221e05e14c111
서호준 <henryseo1000@gmail.com>
2024-01-06 16:38:39 +0900

    Canceled ESPServer Proj

--- SimpleLogin/server/app.py
+++ SimpleLogin/server/app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request
-import pymysql
+import pymysql, ssl
 
 app = Flask(__name__)
 
 # MySQL 데이터베이스 연결
-db = pymysql.connect(host='127.0.0.1', user='root', db='dev_test', charset='utf8')
+db = pymysql.connect(host='192.168.0.34', user='newuser', password='1', db='testdb', charset='utf8')
 
 # 데이터에 접근
 cursor = db.cursor()
@@ -23,11 +23,22 @@ def kakaoMap():
 
 @app.route('/login')
 def tryLogin():
-   if request.method == 'POST' :
+   if request.method == 'GET' :
       result = request.form
       sql = "select id from login where id == " + result.name
       db.query(sql)
+   return render_template('introduce.html')
+
+@app.route('/signup')
+def trySignUp():
+   if request.method == 'POST' :
+      result = request.form
+      sql = "insert into login(id, pwd) values(" + result.name + ',' + result.pwd + ")"
+      db.query(sql)
    return render_template('index.html')
 
+
 if __name__ == '__main__':  
-   app.run('127.0.0.1', port=5001, debug=True)
+   ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+   ssl_context.load_cert_chain(certfile = 'newcert.pem', keyfile = 'newkey.pem', password = 'secret')
+   app.run('127.0.0.1', port = 5001, debug = True, ssl_context = ssl_context)
