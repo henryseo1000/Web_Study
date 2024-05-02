bc509652213e31ec901fc192d884ad1750ad2b1d
서호준 <henryseo1000@gmail.com>
2024-05-03 04:32:17 +0900

    Card_Test done

--- Card_Test/script.js
+++ Card_Test/script.js
@@ -29,23 +29,6 @@ card3.addEventListener('mouseup', function(){
   }
 })
 
-
-var card4 = document.getElementById('move2');
-var move_mode = false;
-card4.addEventListener('mouseup', function(e){
-  // 절대 위치를 구한다
-  var targetTop = card4.getBoundingClientRect().top;
-  var targetBottom = card4.getBoundingClientRect().bottom;
-  var targetLeft = card4.getBoundingClientRect().left;
-  var targetRight = card4.getBoundingClientRect().right;
-
-  console.log(targetTop + ' ' + targetBottom + ' ' + targetLeft + ' ' + targetRight);
-  move_mode = !move_mode;
-  if (move_mode){
-    card4.style = `z-index: 99; transition: 3s ease-in-out`
-  }
-})
-
 // I Just Rather Be Alone
 var alone = document.getElementById('alone');
 alone.addEventListener('mouseup', function(){
@@ -57,43 +40,3 @@ alone.addEventListener('mouseup', function(){
   }
   disapper().then(()=>{setTimeout(forever, 2000)})
 })
-
-// I Believe I Can Touch The Sky
-var drag = document.getElementById('drag');
-
-let init_x = drag.style.offsetLeft
-let init_y = drag.style.offsetTop
-let click_x = 0;
-let click_y = 0;
-let cur_x = 0;
-let cur_y = 0;
-
-const mouseDownHandler = (e) => {
-  click_x = e.offsetX;
-  click_y = e.offsetY;
-
-  drag.addEventListener('mouseup', mouseUpHandler)
-  drag.addEventListener('mousemove', mouseMoveHandler)
-}
-
-const mouseMoveHandler = (e) => {
-  cur_x = e.offsetX;
-  cur_y = e.offsetY;
-
-  console.log("x: " + cur_x + "y: " + cur_y);
-  drag.style.position = 'absolute'
-  drag.style.top = `${init_x + cur_x - click_x}px`;
-  drag.style.left = `${init_y + cur_y - click_y}px`;
-}
-
-const mouseUpHandler = () => {
-  drag.style.position = ''
-  drag.style.top = `${drag.offsetTop + cur_x - click_x}px`;
-  drag.style.left = `${drag.offsetLeft + cur_y - click_y}px`;
-
-  drag.removeEventListener('mouseup', mouseUpHandler)
-  drag.removeEventListener('mousedown', mouseMoveHandler)
-  drag.addEventListener('mousedown', mouseDownHandler)
-}
-
-drag.addEventListener('mousedown', mouseDownHandler)
