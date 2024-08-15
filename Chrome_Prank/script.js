bb35c5bc83fc51a3745424029e01bc6a5ae6740b
서호준 <henryseo1000@gmail.com>
2024-08-15 16:51:30 +0900

    delete cached

--- Chrome_Prank/script.js
+++
@@ -1,8 +0,0 @@
-let imgs = document.querySelectorAll('img');
-let videos = document.querySelectorAll('video')
-imgs.forEach((a, i) => {
-  a.src = 'https://cdn.pixabay.com/photo/2024/03/02/13/05/orange-parrots-8608540_1280.jpg'
-});
-videos.forEach((a, i) => {
-  a.playbackRate = 16
-})
