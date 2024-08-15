a67d0dc92c64c5ea46b8489640a7f0fb18ca8f95
서호준 <henryseo1000@gmail.com>
2024-08-15 23:20:05 +0900

    dynamic routing

--- make.sh
+++ make.sh
@@ -7,4 +7,19 @@ touch static/$1/style.css static/$1/script.js
 
 cd $PWD/templates/$1
 
-touch readme.md index.html
+touch $1.md index.html
+
+echo "<!DOCTYPE html>\n
+<html lang=\"en\">\n
+<head>\n
+    <meta charset="UTF-8">\n
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n
+    <link rel=\"stylesheet\" href=\"{{ url_for('static', filename='$1/global.css')}}\">\n
+    <title>Web Study | By Henry</title>
+</head>
+<body>
+  
+</body>
+</html>
+
+<script src=\"{{ url_for('static', filename='$1/script.js')}}\"></script>" | tee index.html > 'dev/null'
--- server.py
+++ server.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template;
+import os;
 
-app = Flask("Web Study");
+app = Flask("__name__");
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+path = "./templates";
+file_list = os.listdir(path);
 
-@app.route("/card_test")
-def card_test():
-    return render_template("/card_test/index.html")
+# print ("file_list: {}".format(file_list));
+
+@app.route("/<string:path>")
+def route(path):
+    return render_template("/" + path + "/index.html");
 
 if __name__ == '__main__':  
    app.run('127.0.0.1', port=5001, debug=True)
