ac58ff98eba1f0d710db8bb99aa8f374f61d8757
서호준 <henryseo1000@gmail.com>
2024-08-26 02:20:04 +0900

    Made Simple slider with eventlistener

---
+++ static/input_custom_slider/script.js
@@ -0,0 +1,30 @@
+window.onload = function() {
+
+    const slider = document.getElementById('slider');
+    const imgContainer = document.getElementById('img_container');
+
+    let idx = slider.value;
+    let sliderWidth = imgContainer.clientWidth;
+    let moveAmount = idx * sliderWidth;
+
+    function sliderChange () {
+        idx = slider.value;
+        moveAmount = idx * sliderWidth;
+        console.log(moveAmount);
+    }    
+
+    slider.onchange = (e) => sliderChange();
+
+    document.addEventListener('resize', (e) => {
+        sliderWidth = imgContainer.clientWidth;
+        moveAmount = idx * sliderWidth;
+        imgContainer.style.transform = `translateX(-${moveAmount}px)`;
+    })
+
+    document.addEventListener('click', (e) => {
+        e.preventDefault();
+        imgContainer.style.transform = `translateX(-${moveAmount}px)`;
+    });
+}
+
+
