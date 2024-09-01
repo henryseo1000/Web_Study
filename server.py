c2760d93c84cec269564afa410a813d28cca39c4
서호준 <henryseo1000@gmail.com>
2024-09-02 02:24:35 +0900

    fixed favicon Error

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template;
+from flask import Flask, render_template, url_for, redirect;
 import os;
 
 app = Flask("__name__");
@@ -8,6 +8,10 @@ file_list = os.listdir(path);
 
 # print ("file_list: {}".format(file_list));
 
+def redirect_to ():
+    return url_for('static', filename='favicon/favicon.ico');
+
+
 @app.route("/")
 def index():
     return render_template("index.html");
@@ -15,6 +19,8 @@ def index():
 @app.route("/<string:path>")
 def route(path):
     return render_template("/" + path + "/index.html");
+
+app.add_url_rule('/favicon.ico', 'redirect_to', redirect_to);
 
 if __name__ == '__main__':  
    app.run('127.0.0.1', port=5001, debug=True)
